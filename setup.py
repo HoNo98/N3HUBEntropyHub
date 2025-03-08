14e3bcd0985d888dc4f27c038b50bdf6d9a0b067
Hossein <hnowrozinezhad@gmail.com>
2025-03-08 16:45:28 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="N3HUBEntropyHub",
     version="0.1.0",
     description="A package for surrogate-based entropy analysis using EntropyHub on NWB spike data.",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Hossein Nowrozi-Nezhad",
+    author_email="hnowrozinezhad@gmail.com",
     url="https://github.com/HoNo98/N3HUBEntropyHub",
     packages=find_packages(),
     install_requires=[
